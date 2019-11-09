582f89a1e6a7132d7ca7fcb1529ddd0a58ed0178
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-11-09 13:33:13 -0700

    jump finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,45 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    public float speed = 170f;
-    public Vector2 maxVelocity = new Vector2(70, 110);
+public class Player : MonoBehaviour {
 
     private Rigidbody2D body2D;
-    private SpriteRenderer renderer2D;
+    public float speed = 170f;
+    private float moveInput;
+    public float jumpForce = 100f;
+
+    public bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
     {
         body2D = GetComponent<Rigidbody2D>();
-        renderer2D = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate() {
+        moveInput = Input.GetAxis("Horizontal");
+        body2D.velocity = new Vector2(moveInput * speed, body2D.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var absVelX = Mathf.Abs(body2D.velocity.x);
 
-        var forceX = 0f;
-        var forceY = 0f;
-
-        if (Input.GetKey("right")) {
-            if (absVelX < maxVelocity.x) {
-                forceX = speed;
-            }
-
-            renderer2D.flipX = false;
-
-        }
-        else if (Input.GetKey("left")) {
-            if (absVelX < maxVelocity.x) {
-                forceX = -speed;
-            }
-
-            renderer2D.flipX = true;
+        // jump
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (moveInput > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else if (moveInput < 0) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
         }
 
-        body2D.AddForce(new Vector2(forceX, forceY));
+        if (isGrounded == true && (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))) {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            body2D.velocity = Vector2.up * jumpForce;
+        }
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && isJumping == true) {
+            if (jumpTimeCounter > 0) {
+                body2D.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+
+
+        }
+
+        if ((Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)))
+            isJumping = false;
     }
+
 }
