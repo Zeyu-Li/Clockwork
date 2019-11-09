27df586514eaa316add66e8c274d8ee51a91208c
Allan Manuba <allanmanuba@gmail.com>
2019-11-09 16:16:53 -0700

    Add soul change without force

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
     private Rigidbody2D body2D;
     public float speed = 140f;
     private float moveInput;
@@ -18,9 +17,29 @@ public class Player : MonoBehaviour {
     public float jumpTime;
     private bool isJumping;
 
+    [Header("Soul components")]
+    [Tooltip("Check out PHYSICS2D layer")]
+    public int defaultLayer = 0;
+    [Tooltip("Check out PHYSICS2D layer")]
+    public int soulLayer = 6;
+    [Tooltip("Check out PHYSICS2D layer")]
+    public int transitionLayer = 7;
+    public Sprite defaultSprite;
+    public Sprite noSoulBodySprite;
+    public Transform noSoulBody;
+    public bool isSoulTime;
+    public bool isTransitioning;
+    public float transitionDuration = 2.0f;
+
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
+        isSoulTime = false;
+        isTransitioning = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = defaultSprite;
         body2D = GetComponent<Rigidbody2D>();
     }
 
@@ -32,6 +51,8 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (isTransitioning)
+            return;
 
         // jump
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
@@ -59,6 +80,59 @@ public class Player : MonoBehaviour {
 
         if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)))
             isJumping = false;
+
+        if (Input.GetButtonDown("ToggleTime") && !isTransitioning)
+        {
+            if (isSoulTime)
+            {
+                // push player here
+                gameObject.layer = transitionLayer;
+                StartCoroutine(GoToSoul());
+            }
+            else
+            {
+                // change player sprite
+                spriteRenderer.sprite = noSoulBodySprite;
+                noSoulBody.transform.position = transform.position;
+                noSoulBody.gameObject.SetActive(true);
+                // change physics layer for object
+                gameObject.layer = soulLayer;
+                // move
+            }
+
+            isSoulTime = !isSoulTime;
+        }
     }
 
+    IEnumerator GoToSoul()
+    {
+        isTransitioning = true;
+        body2D.isKinematic = true;
+        Vector2 startPosition = transform.position;
+        float startTime = Time.time;
+        float endTime = startTime + transitionDuration;
+        Debug.Log("Our time is " + startTime.ToString() + " vs " + endTime.ToString());
+
+        while (Time.time < endTime)
+        {
+            float change = Time.time - startTime;
+            transform.position = Vector2.Lerp(startPosition, noSoulBody.position, change / transitionDuration);
+            yield return new WaitForSeconds(1.0f/60.0f);
+        }
+
+        transform.position = noSoulBody.position;
+
+        // todo: push the body here???
+
+        // change physics layer for object
+        // change player sprite
+        spriteRenderer.sprite = defaultSprite;
+        noSoulBody.gameObject.SetActive(false);
+        // change physics layer for object
+        gameObject.layer = defaultLayer;
+
+        isTransitioning = false;
+        body2D.isKinematic = false;
+        yield return null;
+    }
 }
