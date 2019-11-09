46a5b82f4e1006834ca4cbcc0c043a14b045badb
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-11-08 23:21:29 -0700

    left and right player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 170f;
+    public Vector2 maxVelocity = new Vector2(70, 110);
+
+    private Rigidbody2D body2D;
+    private SpriteRenderer renderer2D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body2D = GetComponent<Rigidbody2D>();
+        renderer2D = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var absVelX = Mathf.Abs(body2D.velocity.x);
+
+        var forceX = 0f;
+        var forceY = 0f;
+
+        if (Input.GetKey("right")) {
+            if (absVelX < maxVelocity.x) {
+                forceX = speed;
+            }
+
+            renderer2D.flipX = false;
+
+        }
+        else if (Input.GetKey("left")) {
+            if (absVelX < maxVelocity.x) {
+                forceX = -speed;
+            }
+
+            renderer2D.flipX = true;
+        }
+
+        body2D.AddForce(new Vector2(forceX, forceY));
     }
 }
