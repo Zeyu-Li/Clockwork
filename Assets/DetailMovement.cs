7b2a4dda89f0735338d64f56f683d148fe45693a
Applepiesleth <30684104+Applepiesleth@users.noreply.github.com>
2019-11-10 11:01:27 -0700

    Created pendulum

--- Assets/DetailMovement.cs
+++ Assets/DetailMovement.cs
@@ -6,13 +6,18 @@ public class DetailMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private HingeJoint2D hinge;
+    private JointMotor2D motor;
     public Player player;
+    public int type;
+
+    private float counter;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         hinge = GetComponent<HingeJoint2D>();
+        counter = 0f;
     }
 
     // Update is called once per frame
@@ -27,6 +32,13 @@ public class DetailMovement : MonoBehaviour
         {
             rb.freezeRotation = false;
             hinge.useMotor = true;
+            if (type == 1)
+            {
+                counter += 0.02f;
+                transform.Rotate(0, 0, Mathf.Cos(counter));
+            }
         }
+
+        
     }
 }
