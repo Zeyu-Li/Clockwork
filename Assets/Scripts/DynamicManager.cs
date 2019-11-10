8364076f57399aa6a7a9dcbcc5e671256bfbb034
Peripuff <57550053+Peripuff@users.noreply.github.com>
2019-11-10 11:45:45 -0700

    Dynamic Music!

--- Assets/Scripts/DynamicManager.cs
+++ Assets/Scripts/DynamicManager.cs
@@ -10,15 +10,21 @@ public class DynamicManager : MonoBehaviour
     public AudioSource audioSource;
 
     // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(audioSource);
+    }
+
     void Start()
     {
-        player = GetComponent<Player>();
+        player = GameObject.FindGameObjectWithTag("player").GetComponent<Player>();
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(player.isSoulTime);
         if (player.isSoulTime && soul)
         {
             audioSource.mute = false;
