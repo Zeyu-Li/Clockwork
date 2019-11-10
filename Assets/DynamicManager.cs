4a40d1b3a1f34b032bc5b6fa064d3097a488ba45
Peripuff <57550053+Peripuff@users.noreply.github.com>
2019-11-10 11:27:33 -0700

    Moar Music

---
+++ Assets/DynamicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicManager : MonoBehaviour
+{
+
+    public bool soul;
+    public Player player;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.isSoulTime && soul)
+        {
+            audioSource.mute = false;
+        }
+        else if (!player.isSoulTime && !soul)
+        {
+            audioSource.mute = false;
+        }
+        else
+        {
+            audioSource.mute = true;
+        }
+    }
+}
