a6f4c2648885764a7a02c15c65fcbb9e4eb046eb
Applepiesleth <30684104+Applepiesleth@users.noreply.github.com>
2019-11-10 01:03:33 -0700

    Made moving gears stop during time freeze

---
+++ Assets/DetailMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private HingeJoint2D hinge;
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        hinge = GetComponent<HingeJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.isSoulTime)
+        {
+            rb.freezeRotation = true;
+            hinge.useMotor = false;
+        } 
+        else
+        {
+            rb.freezeRotation = false;
+            hinge.useMotor = true;
+        }
+    }
+}
