5d3ed3fce036d14f67f29f8d9583ce0ed745c6b6
Applepiesleth <30684104+Applepiesleth@users.noreply.github.com>
2019-11-10 11:19:02 -0700

    Implemented Tint

---
+++ Assets/Tinter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tinter : MonoBehaviour
+{
+
+    public Player player;
+    private Color col;
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+         sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        col = sr.color;
+        if (player.isSoulTime)
+        {
+            if (col.a < 0.4)
+            {
+                col.a += 0.02F;
+            }
+            
+        }
+        else
+        {
+            if (col.a > 0.0)
+            {
+                col.a -= 0.02F;
+            }
+        }
+
+        sr.color = col;
+    }
+}
